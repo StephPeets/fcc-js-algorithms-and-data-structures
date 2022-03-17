8a383fec327cdeafd45f9dbd4f7f35ab801863b5
Steph Peets <stephanie.peets@gmail.com>
2022-03-17 00:12:50 -0400

    roman numeral converter

--- functional-programming/REDO-21-apply-functional-programming-to-convert-strings-to-url-slugs.js
+++ functional-programming/REDO-21-apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -2,7 +2,11 @@
 
 Apply Functional Programming to Convert Strings to URL Slugs
 
-Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. You can use any of the methods covered in this section, and don't use replace. Here are the requirements:
+Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. 
+
+You can use any of the methods covered in this section, and don't use replace. 
+
+Here are the requirements:
 
 The input is a string with spaces and title-cased words
 
@@ -27,10 +31,19 @@ function urlSlug(title) {
        finalTitle.push(titleToUrl[i])
      }
     }
-    
+
     return finalTitle.join("-");
     }
-    // Only change code above this line
-    
-    console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
-    console.log(urlSlug(" Winter Is  Coming"));
+// Only change code above this line
+
+console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+console.log(urlSlug(" Winter Is  Coming"));
+
+// ANSWER BELOW:
+
+// function urlSlug(title) {
+
+//   let outputStr = title.toLowerCase().trim().split(" ").filter(word => word.length > 0).join("-");
+
+//   return outputStr;
+// }
