9bfd679f3762c483a334dd03ed843da19afeabbb
Steph Peets <stephanie.peets@gmail.com>
2022-03-16 21:12:09 -0400

    Apply Functional Programming to Convert Strings to URL Slugs

---
+++ functional-programming/REDO-21-apply-functional-programming-to-convert-strings-to-url-slugs.js
@@ -0,0 +1,36 @@
+/*
+
+Apply Functional Programming to Convert Strings to URL Slugs
+
+Fill in the urlSlug function so it converts a string title and returns the hyphenated version for the URL. You can use any of the methods covered in this section, and don't use replace. Here are the requirements:
+
+The input is a string with spaces and title-cased words
+
+The output is a string with the spaces between words replaced by a hyphen (-)
+
+The output should be all lower-cased letters
+
+The output should not have any spaces
+
+*/
+
+// REDO: complete using regex and an additional method or two (not replace/replaceAll)
+
+// Only change code below this line
+function urlSlug(title) {
+    let finalTitle = [];
+    let titleToUrl = title
+      .toLowerCase()
+      .split(" ")
+    for (let i = 0; i < titleToUrl.length; i++) {
+     if (titleToUrl[i].length) {
+       finalTitle.push(titleToUrl[i])
+     }
+    }
+    
+    return finalTitle.join("-");
+    }
+    // Only change code above this line
+    
+    console.log(urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone"));
+    console.log(urlSlug(" Winter Is  Coming"));
