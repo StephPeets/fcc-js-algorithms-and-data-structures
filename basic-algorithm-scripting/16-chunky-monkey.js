8439aa5e934dfc07bafb07df01abb1fab76e117b
Steph Peets <stephanie.peets@gmail.com>
2022-03-16 21:27:32 -0400

    refactor chunky monkey

--- basic-algorithm-scripting/16-chunky-monkey.js
+++ basic-algorithm-scripting/16-chunky-monkey.js
@@ -8,6 +8,22 @@ https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/bas
 
 */
 
+// Refactored on 3/16/22
+
+function chunkArrayInGroups(arr, size) {
+  let twoDSplitArr = [];
+  let origArr = arr;
+  while (origArr.length > 0) {
+    let addOn = origArr.splice(0, size);
+    twoDSplitArr.push(addOn);
+  }
+
+  return twoDSplitArr;
+}
+
+/* 
+From 3/15/22:
+
 function chunkArrayInGroups(arr, size) {
   let chunkyArr = [];
   let leftOverChunk = arr.length % size;
@@ -22,6 +38,8 @@ function chunkArrayInGroups(arr, size) {
 
   return chunkyArr;
 }
+
+*/
 
 console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2), `  <-- Answer s/b: [ [ 'a', 'b' ], [ 'c', 'd' ] ]\n`);
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4), `  <-- Answer s/b: [ [ 0, 1, 2, 3 ], [ 4, 5, 6, 7 ], [ 8 ] ]\n`); // 
